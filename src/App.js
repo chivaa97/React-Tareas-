002ab9c6a032448d5df256df660c15bc9ab33fd4
chivaa97 <ceinnova12242@innovaccion.mx>
2022-06-24 22:04:59 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import logo from "./imagenes/FCCL.png"
 import './App.css';
+import ListaDeTareas from "./componentes/ListaDeTareas";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="aplicacion-tareas">
+      <div className="freecodecamp-logo-contenedor">
+        <img className="freecodecamp-logo"
+        src={logo} alt="logo" />
+      </div>
+      <div className="tareas-lista-principal">
+        <h1>Mis Tareas</h1>
+        {/* <Tarea texto="Aprender  React" /> */}
+        {/* <TareaFormulario /> */}
+        <ListaDeTareas />
+      </div> 
+     </div>
   );
 }
 
